444922bceaee9658534923ef122b81378e476cea
Felix Krull <f_krull@gmx.de>
2015-01-26 22:35:29 +0100

    Update minimal modules.
    --HG--
    branch : trusty

--- debian/pymindeps.py
+++ debian/pymindeps.py
@@ -123,6 +123,7 @@ excluded_imports = {
     'tempfile': set(('dummy_thread',)),
     'subprocess': set(('threading',)),
     'shutil': set(('distutils', 'tarfile', 'zipfile')),
+    'ssl': set(('calendar',)),
     'sysconfig': set(('pprint', '_osx_support')),
     }
 
