680f213d43484e42351f697a71a650bd66200816
Felix Krull <f_krull@gmx.de>
2015-12-12 21:20:52 +0100

    Update minimal modules.
    The ctypes import in platform is only on Windows, so no concern to us.
    
    --HG--
    branch : trusty

--- debian/pymindeps.py
+++ debian/pymindeps.py
@@ -118,7 +118,7 @@ excluded_imports = {
                'riscos', 'riscospath', 'riscosenviron')),
     'optparse': set(('gettext',)),
     'pickle': set(('doctest',)),
-    'platform': set(('plistlib', 'tempfile')),
+    'platform': set(('ctypes', 'plistlib', 'tempfile')),
     #'socket': set(('_ssl', 'ssl')),
     'tempfile': set(('dummy_thread',)),
     'subprocess': set(('threading',)),
